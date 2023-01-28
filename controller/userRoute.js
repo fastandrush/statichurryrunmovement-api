65edf7a1c6914f1b6cd9ddc94b7a9bb082a7327b
fasterrush <fasterrush@gmail.com>
2023-01-29 00:28:12 +0800

    first commit

--- controller/userRoute.js
+++ controller/userRoute.js
@@ -697,9 +697,8 @@ Router.route('/getcurrentlyloginmacuser').post(async (req, res)=> {
         }
       }
 
-      await mongoose.connection.close()
       res.status(200).send(_currentLoginUserData)
-      
+
      } catch(err) {
        console.log('Error getting currentlyloginuser' + ' ' + err)
      } finally {
